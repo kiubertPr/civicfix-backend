cc9a4289e7e462809e6b7cade28a276bf5664d96
Roberto Piñon <roberto201090@gmail.com>
2026-06-29 15:23:20 +0200

    feat: actualizar patrones de origen permitidos en la configuración CORS

--- src/main/java/com/civicfix/tfg/rest/common/SecurityConfig.java
+++ src/main/java/com/civicfix/tfg/rest/common/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
 		config.setAllowCredentials(true);
-		config.setAllowedOriginPatterns(Arrays.asList("http://localhost:5173","https://*.vercel.app"));
+		config.setAllowedOrigins(Arrays.asList( "http://localhost:5173", "https://civicfix.kiubert.es"));
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("*");
 
